c271ba8be0529c0740d5b7812477f29fba5c0ceb
Ashmin <ashmin.pathak@gmail.com>
2020-01-05 19:31:12 -0500

    adds a simple node server

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  next();
+});
+
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: "hdsfhdsja13123",
+      title: "First From Node",
+      content: "First Content from Node"
+    },
+    {
+      id: "hdsfhdsja13123",
+      title: "Second From Node",
+      content: "Second Content from Node"
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
+});
+
+module.exports = app;
